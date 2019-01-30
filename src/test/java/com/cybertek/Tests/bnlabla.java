a5eac507cd6ec7cb93e2fe64e981e679f045e982
kurshat0920 <46765765+kurshat0920@users.noreply.github.com>
2019-01-30 18:28:23 -0500

    Update bnlabla.java

--- src/test/java/com/cybertek/Tests/bnlabla.java
+++ src/test/java/com/cybertek/Tests/bnlabla.java
@@ -3,5 +3,8 @@ package com.cybertek.Tests;
 public class bnlabla {
     public static void main(String[] args) {
         System.out.println("hello");
+        
+        //edit from github
+        
     }
 }
